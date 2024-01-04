d2be4bbd7e9d56baca8eec897fb2d5fba6b1ad44
Samuel Gonzalez <Nomekop65@yahoo.com>
2024-01-04 11:55:05 -0500

    Only need winning player logic

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 
 let player1Choice = ' '
+let player2Choice = ' '
 
 const player1RockButton = document.querySelector('.player1 .fa-hand-back-fist')
 const player1PaperButton = document.querySelector('.player1 .fa-hand')
@@ -22,28 +23,46 @@ console.log(player2ScissorsButton)
 function clickOnPlayer1RockButton() {
   player1Choice = 'rock'
   console.log(`Player one's choice is ${player1Choice}!`)
+  readyToPlay()
 }
 function clickOnPlayer1PaperButton() {
   player1Choice = 'paper'
   console.log(`Player one's choice is ${player1Choice}!`)
+  readyToPlay()
 }
 function clickOnPlayer1ScissorsButton() {
   player1Choice = 'scissors'
   console.log(`Player one's choice is ${player1Choice}!`)
+  readyToPlay()
 }
 function clickOnPlayer2RockButton() {
-  player1Choice = 'rock'
-  console.log(`Player two's choice is ${player1Choice}!`)
+  player2Choice = 'rock'
+  console.log(`Player two's choice is ${player2Choice}!`)
+  readyToPlay()
 }
 function clickOnPlayer2PaperButton() {
-  player1Choice = 'paper'
-  console.log(`Player two's choice is ${player1Choice}!`)
+  player2Choice = 'paper'
+  console.log(`Player two's choice is ${player2Choice}!`)
+  readyToPlay()
 }
 function clickOnPlayer2ScissorsButton() {
-  player1Choice = 'scissors'
-  console.log(`Player two's choice is ${player1Choice}!`)
+  player2Choice = 'scissors'
+  console.log(`Player two's choice is ${player2Choice}!`)
+  readyToPlay()
 }
-
+function readyToPlay() {
+  if (player1Choice != ' ' && player2Choice != ' ') {
+    console.log('We are ready to play!')
+    weHaveAWinner()
+  } else {
+    console.log('We are not ready')
+  }
+}
+function weHaveAWinner() {
+  console.log(player1Choice)
+  console.log(player2Choice)
+  console.log('We have a winner!')
+}
 player1RockButton?.addEventListener('click', clickOnPlayer1RockButton)
 player1PaperButton?.addEventListener('click', clickOnPlayer1PaperButton)
 player1ScissorsButton?.addEventListener('click', clickOnPlayer1ScissorsButton)
